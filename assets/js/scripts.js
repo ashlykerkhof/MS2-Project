4735817147bd03df6ac772896be76374f653674f
ashlykerkhof <ashly.kerkhof@outlook.com>
2021-01-26 14:36:25 +0000

    outline of memory game done before starting on javascript

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const  cardGame = ["aragon.jpg", "aragon.jpg", "gandalf.jpg", "gandalf.jpg", "gollum.jpg", "gollum.jpg", "gimli.jpg", "gimli.jpg", "legolas.jpg", "legolas.jpg", "orc.jpg", "orc.jpg", "sauron.jpg", "sauron.jpg", "pippin.jpg", "pippin.jpg", "frodo.jpeg", "frodo.jpeg", "nazgul.jpg", "nazgul.jpg",]
+
+const deck = document.querySelector(".memory-game")
+
+let opened = [];
+
+let matched = [];
+
+const modal = document.getElementById("modal")
